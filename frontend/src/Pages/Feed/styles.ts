8996b5f9b9a130b267993b1520a4b4bcaabaed7f
Rogerio Orioli <rogerio@designhope.com.br>
2020-08-16 13:22:42 -0300

    litte adjusts

--- frontend/src/Pages/Feed/styles.ts
+++ frontend/src/Pages/Feed/styles.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.main`
     max-height : 700px;
     width : 100%;
-    padding : 60px 0;
+    padding: 100px 0 60px;
     overflow : scroll;
+    display: flex;
+    align-items : center;
+    justify-content : center;
+    flex-wrap: wrap;
 `;
--- frontend/src/Pages/Profile/styles.ts
+++ frontend/src/Pages/Profile/styles.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-    max-height : 700px;
+   max-height : 700px;
     width : 100%;
-    padding : 60px 0;
+    padding:60px 0;
     overflow : scroll;
+    display: flex;
+    align-items : center;
+    justify-content : center;
+    flex-wrap: wrap;
     h2 {
         text-align : center;
-        padding : 4% 0; 
+        padding : 4% 0;
+        width : 100%; 
     }
 `;
